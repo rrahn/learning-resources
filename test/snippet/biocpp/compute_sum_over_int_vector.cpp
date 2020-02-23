8f9220a2d3622b51f3c769ae58b248ba2cb4e0b2
rrahn <rene.rahn@fu-berlin.de>
2020-02-24 00:16:58 +0100

    Using views as alternative to compute sum.

--- test/snippet/biocpp/compute_sum_over_int_vector.cpp
+++ test/snippet/biocpp/compute_sum_over_int_vector.cpp
@@ -16,12 +16,28 @@ int32_t sum_short(std::vector<int32_t> const & data)
     return std::accumulate(data.begin(), data.end(), 0);  // Use algorithms
 }
 
-int main()
+int main(int argc, char const * argv[])
 {
-    std::vector<int32_t> int_data{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    std::vector<int32_t> all_numbers{};
+    all_numbers.reserve(argc - 1);
 
-    std::cout << "The sum of my vector is " << sum(int_data) << "\n";
-    std::cout << "The sum of my vector is " << sum_short(int_data) << "\n";
+    for (int index = 1; index < argc; ++index)
+        all_numbers.push_back(std::atoi(argv[index]));
+
+    std::cout << "The sum of all numbers is " << sum(all_numbers) << "\n";
 
     return EXIT_SUCCESS;
 }
+
+/* Solution with ranges. Could be shown later as well.
+int main(int argc, char const * argv[])
+{
+    auto all_numbers = std::views::counted(std::addressof(argv[1]), argc - 1)
+                     | std::views::transform([] (auto && c_string) { return std::atoi(c_string); })
+                     | seqan3::views::to<std::vector>;
+
+    std::cout << "The sum of all numbers is " << sum(all_numbers) << "\n";
+
+    return EXIT_SUCCESS;
+}
+*/
